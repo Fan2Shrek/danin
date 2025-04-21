218186bb312962ba7437bd5087f873402935da20
Pierre Ambroise <74421318+Fan2Shrek@users.noreply.github.com>
2025-04-21 18:56:44 +0200

    Add tests on login (#5)

---
+++ api/tests/Functional/User/LoginTest.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Functional\User;
+
+use App\Tests\Functional\FunctionalTestCase;
+
+final class LoginTest extends FunctionalTestCase
+{
+    use UserTrait;
+
+    public function testLogin()
+    {
+        $this->createUser(
+            email: 'extras@gmail.com',
+            password: 'secret',
+        );
+
+        $response = $this->client->request('POST', '/api/login', [
+            'json' => [
+                'email' => 'extras@gmail.com',
+                'password' => 'secret',
+            ]
+        ]);
+
+        self::assertResponseIsSuccessful();
+        self::assertJsonHasKey('token', $response);
+    }
+
+    public function testLoginWrongCredentials()
+    {
+        $this->createUser(
+            email: 'extras@gmail.com',
+            password: 'secret',
+        );
+
+        $this->client->request('POST', '/api/login', [
+            'json' => [
+                'email' => 'fake@gmail.com',
+                'password' => 'wrong',
+            ]
+        ]);
+
+        self::assertResponseStatusCodeSame(401);
+        self::assertJsonContains([
+            'message' => 'Invalid credentials.',
+        ]);
+    }
+}
--- api/tests/Functional/User/MeTest.php
+++ api/tests/Functional/User/MeTest.php
@@ -4,34 +4,11 @@ declare(strict_types=1);
 
 namespace App\Tests\Functional\User;
 
-use ApiPlatform\Symfony\Bundle\Test\ApiTestCase;
-use ApiPlatform\Symfony\Bundle\Test\Client;
-use App\Entity\User;
-use Doctrine\DBAL\Connection;
-use Doctrine\ORM\EntityManagerInterface;
-use Doctrine\Persistence\ManagerRegistry;
+use App\Tests\Functional\FunctionalTestCase;
 
-final class MeTest extends ApiTestCase
+final class MeTest extends FunctionalTestCase
 {
-    protected static ?bool $alwaysBootKernel = true;
-    protected Client $client;
-
-    public function setup(): void
-    {
-        $this->client = self::createClient();
-        $this->client->disableReboot();
-
-        static::getContainer()->get(Connection::class)->beginTransaction();
-
-        parent::setUp();
-    }
-
-    public function tearDown(): void
-    {
-        static::getContainer()->get(Connection::class)->rollBack();
-
-        parent::tearDown();
-    }
+    use UserTrait;
 
     public function testMe()
     {
@@ -79,29 +56,8 @@ final class MeTest extends ApiTestCase
         $user = $this->createUser();
         $this->client->loginUser($user);
 
-        $this->client->request('GET', '/api/me');
+        $response = $this->client->request('GET', '/api/me');
 
-        self::assertArrayNotHasKey('password', $this->client->getResponse()->toArray(false));
-    }
-
-    public static function getEM(): EntityManagerInterface
-    {
-        return static::getContainer()->get(ManagerRegistry::class)->getManager();
-    }
-
-    private function createUser(
-        string $username = 'default',
-        string $email = 'default@gmail.com',
-    ): User
-    {
-        $user = new User($username, $email);
-        $user->setPassword('password');
-        $user->setRoles(['ROLE_USER']);
-
-        $this->getEm()->persist($user);
-        $this->getEm()->flush();
-
-        return $user;
-
+        self::assertJsonHasNotKey('password', $response);
     }
 }
