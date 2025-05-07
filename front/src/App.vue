d6c4ba964da7cb0226b9eab0e3b970898b8a99cd
Pierre Ambroise <pierre27.ambroise@gmail.com>
2025-05-07 19:25:41 +0200

    Add user provider

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,12 +1,15 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router';
 
+import UserProvider from '@/components/UserProvider.vue';
 import LanguageSwitcher from '@/components/i18n/LanguageSwitcher.vue';
 import FooterComponent from '@/components/layout/FooterComponent.vue';
 </script>
 
 <template>
-    <LanguageSwitcher />
-    <RouterView />
-    <FooterComponent />
+    <UserProvider>
+        <LanguageSwitcher />
+        <RouterView />
+        <FooterComponent />
+    </UserProvider>
 </template>
