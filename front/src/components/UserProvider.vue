56ce69726d14fbea5c8a4c2346ca3ca7a5eeb682
Pierre Ambroise <pierre27.ambroise@gmail.com>
2025-05-08 21:13:01 +0200

    Redirect to home on login

--- front/src/components/UserProvider.vue
+++ front/src/components/UserProvider.vue
@@ -1,13 +1,17 @@
 <script setup lang="ts">
 import { provide } from 'vue';
+import { useRouter } from 'vue-router';
 
 import api from '@/lib/api/api';
 import { setCookie, deleteCookie } from '@/lib/cookies';
+
+const router = useRouter();
 
 const loginUser = async (username: string, password: string) => {
     const response = await api().user().login(username, password);
     api().setToken(response.token);
     setCookie('token', response.token);
+    router.push({ name: 'Home' });
 };
 
 const logoutUser = () => {
