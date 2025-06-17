faf493d7afab5699d1cc816d35d5f2cf7b8fccf8
Pierre Ambroise <pierre27.ambroise@gmail.com>
2025-06-17 14:42:22 +0200

    Add provider api

---
+++ api/src/Service/Provider/ProviderManager.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Service\Provider;
+
+final class ProviderManager
+{
+    /**
+     * @param iterable<ProviderInterface> $providers
+     */
+    public function __construct(
+        private iterable $providers,
+    ) {
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getAll(): array
+    {
+        $names = [];
+        foreach ($this->providers as $provider) {
+            if (method_exists($provider, 'getName')) {
+                $names[] = $provider->getName();
+            }
+        }
+
+        return $names;
+    }
+
+    public function startAll(): void
+    {
+        foreach ($this->providers as $provider) {
+            $provider->start();
+        }
+    }
+}
---
+++ api/tests/Functional/Game/GetProvidersTest.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Functional\Game;
+
+use App\Tests\Functional\FunctionalTestCase;
+
+final class GetProvidersTest extends FunctionalTestCase
+{
+    protected const URI = '/api/providers';
+    protected static bool $requestsWithAuthentication = true;
+
+    public function testCall()
+    {
+        $this->client->request('GET', static::URI);
+
+        self::assertResponseIsSuccessful();
+    }
+
+    public function testCount()
+    {
+        $response = $this->client->request('GET', static::URI);
+
+        self::assertEquals(1, $response->toArray()['totalItems']);
+    }
+
+    public function testFormat()
+    {
+        $response = $this->client->request('GET', static::URI);
+
+        self::assertArraySubset([
+            'name' => 'Discord',
+        ], $response->toArray()['member'][0]);
+    }
+}
