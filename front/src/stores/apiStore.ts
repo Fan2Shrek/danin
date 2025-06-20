ef2246e5d374b02cd4262f1889d346b0856c1bda
Pierre Ambroise <pierre27.ambroise@gmail.com>
2025-06-20 10:14:35 +0200

    Fix api store with languages

--- front/src/stores/apiStore.ts
+++ front/src/stores/apiStore.ts
@@ -3,11 +3,13 @@
 
 import { ref } from 'vue';
 import { defineStore } from 'pinia';
+import { useI18n } from 'vue-i18n';
 
 import api from '@/lib/api/api';
 
 export const useApiStore = defineStore('store', () => {
     const store = ref({});
+    const i18nLocale = useI18n();
 
     const makeAction = async (action: string, params = []) => {
         if (!storeActions.hasOwnProperty(action)) {
@@ -20,13 +22,20 @@ export const useApiStore = defineStore('store', () => {
     };
 
     const getStoreState = async (key: string) => {
-        if (!store.value.hasOwnProperty(key)) {
-            let params = [];
+        if (!store.value[i18nLocale.locale.value]) {
+            store.value[i18nLocale.locale.value] = {};
+        }
+
+        if (
+            !store.value[i18nLocale.locale.value].hasOwnProperty(key) &&
+            !store.value.hasOwnProperty(key)
+        ) {
+            const params = [i18nLocale.locale.value];
             let actionName = key;
             if (key.includes('_')) {
                 const parts = key.split('_');
                 actionName = parts[0];
-                params = [parts[1]];
+                params.push(parts[1]);
             }
 
             await makeAction(
@@ -35,7 +44,7 @@ export const useApiStore = defineStore('store', () => {
             );
         }
 
-        return store.value[key];
+        return store.value[i18nLocale.locale.value][key] || store.value[key];
     };
 
     return {
@@ -45,11 +54,11 @@ export const useApiStore = defineStore('store', () => {
 });
 
 export const storeActions = {
-    fetchGames: async (store) => {
-        store.games = await api().game().getAll();
+    fetchGames: async (store, [locale]) => {
+        store[locale].games = await api().game().getAll();
     },
-    fetchCommands: async (store, [gameName]) => {
-        store[`commands_${gameName}`] = await api().game().getCommands(gameName);
+    fetchCommands: async (store, [locale, gameName]) => {
+        store[locale][`commands_${gameName}`] = await api().game().getCommands(gameName);
     },
     fetchProviders: async (store) => {
         store.providers = await api().provider().getAll();
