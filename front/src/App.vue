d13acb2344613f3f7dab8795798529d1e3c40a28
CharlesLLM <charlesllm@orange.fr>
2025-07-17 22:39:26 +0200

    lint
    Signed-off-by: CharlesLLM <charlesllm@orange.fr>

--- front/src/App.vue
+++ front/src/App.vue
@@ -9,27 +9,21 @@ import FooterComponent from '@/components/layout/FooterComponent.vue';
 
 <template>
     <Head>
-        <meta property="og:title" content="Danin">
-        <meta property="og:type" content="website">
-        <meta property="og:url" content="https://sruuua.fr">
-        <meta property="og:image" content="https://sruuua.fr/D.png">
-        <meta property="og:description" content="Danin: Découvrez une nouvelle façon de jouer avec vos amis">
-        <meta property="og:site_name" content="Danin">
-        <meta property="og:locale" content="fr_FR">
+        <meta property="og:title" content="Danin" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://sruuua.fr" />
+        <meta property="og:image" content="https://sruuua.fr/D.png" />
+        <meta
+            property="og:description"
+            content="Danin: Découvrez une nouvelle façon de jouer avec vos amis"
+        />
+        <meta property="og:site_name" content="Danin" />
+        <meta property="og:locale" content="fr_FR" />
 
-        <component is="script" type="application/ld+json">
-            {
-                "@context": "https://schema.org",
-                "@graph": [
-                    {
-                        "@type": "WebSite",
-                        "@id": "https://sruuua.fr/",
-                        "name": "Danin",
-                        "url": "https://sruuua.fr/",
-                        "description": "Danin: Découvrez une nouvelle façon de jouer avec vos amis",
-                        "inLanguage": "fr-FR"
-                    }
-                ]
+        <component :is="'script'" type="application/ld+json">
+            { "@context": "https://schema.org", "@graph": [ { "@type": "WebSite", "@id":
+            "https://sruuua.fr/", "name": "Danin", "url": "https://sruuua.fr/", "description":
+            "Danin: Découvrez une nouvelle façon de jouer avec vos amis", "inLanguage": "fr-FR" } ]
             }
         </component>
     </Head>
